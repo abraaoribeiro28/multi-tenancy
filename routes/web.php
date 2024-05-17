39c1ee864f622d8703d56e18449eab260b6d5e3e
Abraão Ribeiro <abraaobeiro@gmail.com>
2024-05-17 16:52:49 -0300

    Adding route::domain

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::domain('{tenancy}.multi-tenancy.test')->group(function () {
+    Route::get('/', function ($tenancy) {
+        return view('welcome');
+    });
 });
